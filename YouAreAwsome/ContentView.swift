ecc662a86d3a0333300030e31deeacd077a9cce3
Moises Lopez <moilpz5160@gmail.com>
2025-02-25 13:47:25 -0800

    finished feb 19 - 21 work

--- YouAreAwsome/ContentView.swift
+++ YouAreAwsome/ContentView.swift
@@ -9,44 +9,61 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var message = ""
-    @State private var imageString = ""
+    @State private var imageName = ""
+    @State private var lastMessageNumber = -1 //lastMessageNumber will never be -1
+    @State private var lastImageNumber = -1
+    
     var body: some View {
         
         VStack {
-            Spacer()
-            Image(systemName: imageString)
-                .resizable()
-                .scaledToFit()
-                .foregroundStyle(.orange)
-            
             Text(message)
                 .font(.largeTitle)
-                .fontWeight(.ultraLight)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+                .multilineTextAlignment(.center)
+                .minimumScaleFactor(0.5)
+                .frame(height: 100)
+                .animation(.easeInOut(duration: 0.15), value: message)
+            
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+                .clipShape(RoundedRectangle(cornerRadius: 40))
+                .shadow(radius: 30)
+                .animation(.default, value: imageName)
             
             Spacer()
             
             
-            Button("Press Me!") {
-                let message1 = "You are Awesome"
-                let message2 = "You are Great"
-                let imageString1 = "hand.thumbsup"
-                let imageString2 = "sun.max.fill"
+            Button("Show Messages!") {
+                let messages = ["You Are Awesome!",
+                                "When the Genius Bar Needs Help, They Call You!",
+                                "You Are Great!",
+                                "You Are Fantastic!",
+                                "Fabulous? That's You!"]
                 
                 
-//                if message == message1{
-//                    message = message2
-//                    imageString = imageString1
-//                } else {
-//                    message = message1
-//                        imageString = imageString2
-//                }
+                // generate a random messageNumber to use as an index
+                // if messageNumber == last MessageNumber {
+                // set messageString to messages[messageNumber]
+                // update the lastMessageNumber with messageNumber
                 
-                message = (message == message1 ? message2 : message1)
-                imageString = (imageString == imageString1 ? imageString2 : imageString1)
+                var messageNumber:Int
+                repeat {
+                    messageNumber = Int.random(in: 0...messages.count-1)
+                }while messageNumber == lastMessageNumber
+                message = messages[messageNumber]
+                lastMessageNumber = messageNumber
+              
+                var imageNumber = Int.random(in: 0...9)
+                while imageNumber == lastImageNumber{
+                    imageNumber = Int.random(in: 0...9)
+                }
+                imageName = "image\(imageNumber)"
+                lastImageNumber = imageNumber
             }
             .buttonStyle(.borderedProminent)
             .font(.title2)
-            .tint(.orange)
         }
         .padding()
         
